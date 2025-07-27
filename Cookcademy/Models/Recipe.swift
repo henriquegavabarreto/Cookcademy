c7cc42ad41a7a463fe8704944712fff36039ec1d
Henrique Gava <henriquegavabarreto@gmail.com>
2025-07-27 16:27:01 -0700

    Creates enum for dish categories

--- Cookcademy/Models/Recipe.swift
+++ Cookcademy/Models/Recipe.swift
@@ -8,10 +8,22 @@
 import Foundation
 
 struct Recipe {
+    var mainInformation: MainInformation
+    var ingredients: [String]
+    var directions: [String]
+}
+
+struct MainInformation {
     var name: String
     var description: String
     var author: String
-    var category: String
-    var ingredients: [String]
-    var directions: [String]
+    var category: Category
+    
+    // defines dish categories
+    enum Category: String, CaseIterable {
+        case breakfast = "Breakfast"
+        case lunch = "Lunch"
+        case dinner = "Dinner"
+        case dessert = "Dessert"
+    }
 }
