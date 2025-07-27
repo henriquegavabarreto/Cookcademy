55f16186aa124a9f09a8672ceca4c914572abfdb
Henrique Gava <henriquegavabarreto@gmail.com>
2025-07-27 16:36:34 -0700

    Adds structs for Ingredient and Direction, and unit to specify ingredient measure unit

--- Cookcademy/Models/Recipe.swift
+++ Cookcademy/Models/Recipe.swift
@@ -9,8 +9,8 @@ import Foundation
 
 struct Recipe {
     var mainInformation: MainInformation
-    var ingredients: [String]
-    var directions: [String]
+    var ingredients: [Ingredient]
+    var directions: [Direction]
 }
 
 struct MainInformation {
@@ -26,4 +26,24 @@ struct MainInformation {
         case dinner = "Dinner"
         case dessert = "Dessert"
     }
+}
+
+struct Ingredient {
+    var name: String
+    var quantity: Double
+    var unit: Unit
+    
+    enum Unit: String, CaseIterable {
+        case oz = "Ounces"
+        case g = "Grams"
+        case cups = "Cups"
+        case tbs = "Tablespoons"
+        case tsp = "Teaspoons"
+        case none = "No units"
+    }
+}
+
+struct Direction {
+    var description: String
+    var isOptional: Bool
 }
