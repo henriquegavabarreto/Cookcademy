47810b36853e369ec353249d9450bb364ff73b8b
Henrique Gava <henriquegavabarreto@gmail.com>
2025-07-27 16:20:58 -0700

    Creates Recipe model outline

---
+++ Cookcademy/Models/Recipe.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe.swift
+//  Cookcademy
+//
+//  Created by Henrique Barreto on 7/27/25.
+//
+
+import Foundation
+
+struct Recipe {
+    var name: String
+    var description: String
+    var author: String
+    var category: String
+    var ingredients: [String]
+    var directions: [String]
+}
