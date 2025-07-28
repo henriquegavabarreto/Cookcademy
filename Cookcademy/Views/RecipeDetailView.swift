ff0c3e85efbf0ecdd9d53c0aee814cd5bcdfda1a
Henrique Gava <henriquegavabarreto@gmail.com>
2025-07-28 16:10:53 -0700

    Add recipe detail view with author, description, and list with section of ingredients and directions

---
+++ Cookcademy/Views/RecipeDetailView.swift
@@ -0,0 +1,60 @@
+//
+//  RecipeDetailView.swift
+//  Cookcademy
+//
+//  Created by Henrique Barreto on 7/28/25.
+//
+
+import SwiftUI
+
+struct RecipeDetailView: View {
+    let recipe: Recipe
+    
+    var body: some View {
+        VStack {
+            // recipe's author
+            HStack {
+                Text("Author: \(recipe.mainInformation.author)")
+                    .font(.subheadline)
+                    .padding()
+                Spacer()
+            }
+            // recipe's description
+            HStack {
+                Text(recipe.mainInformation.description)
+                    .font(.subheadline)
+                    .padding()
+                Spacer()
+            }
+            List {
+                // recipe's ingredients
+                Section(header: Text("Ingredients")) {
+                    ForEach(recipe.ingredients.indices, id: \.self) { index in
+                        let ingredient = recipe.ingredients[index]
+                        Text(ingredient.description)
+                    }
+                }
+                // recipe's directions
+                Section(header: Text("Directions")) {
+                    ForEach(recipe.directions.indices, id: \.self) { index in
+                        let direction = recipe.directions[index]
+                        HStack {
+                            // step number
+                            Text("\(index + 1).").bold()
+                            // optional status followed by direction description
+                            Text("\(direction.isOptional ? "Optional" : "")" + "\(direction.description)")
+                        }
+                    }
+                }
+            }
+        }
+        .navigationTitle(recipe.mainInformation.name)
+    }
+}
+
+#Preview {
+    @State var recipe = Recipe.testRecipes[0]
+    return NavigationView {
+        RecipeDetailView(recipe: recipe)
+    }
+}
