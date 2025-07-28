2472505501eb7cfeca896eacc8e4799adc18ed02
Henrique Gava <henriquegavabarreto@gmail.com>
2025-07-27 17:07:45 -0700

    Add computed properties that return strings for ingredient description and unit

--- Cookcademy/Models/Recipe.swift
+++ Cookcademy/Models/Recipe.swift
@@ -33,6 +33,24 @@ struct Ingredient {
     var quantity: Double
     var unit: Unit
     
+    var description: String {
+        // get simpler readable number based on quantity
+        let formattedQuantity = String(format: "%g", quantity)
+        switch unit {
+            case .none:
+            // if there are no units, return number and ingredient name in singular or plural
+                let formattedName = quantity == 1 ? name : "\(name)s"
+                return "\(formattedQuantity) \(formattedName)"
+            default:
+            // for all other units, change unit string to plural when the quantity is not 1
+                if quantity == 1 {
+                    return "1 \(unit.singularName) \(name)"
+                } else {
+                    return "\(formattedQuantity) \(unit.rawValue) \(name)"
+                }
+        }
+    }
+    
     enum Unit: String, CaseIterable {
         case oz = "Ounces"
         case g = "Grams"
@@ -40,6 +58,9 @@ struct Ingredient {
         case tbs = "Tablespoons"
         case tsp = "Teaspoons"
         case none = "No units"
+        
+        // returns raw value without last letter (S)
+        var singularName: String { String(rawValue.dropLast()) }
     }
 }
 
