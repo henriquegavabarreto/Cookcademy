d7278281effa7ff86920c15c5365f68f89d85e85
Henrique Gava <henriquegavabarreto@gmail.com>
2025-07-29 11:30:30 -0700

    Apply color scheme to RecipesListView

--- Cookcademy/Views/RecipesListView.swift
+++ Cookcademy/Views/RecipesListView.swift
@@ -11,11 +11,17 @@ struct RecipesListView: View {
     // declare recipeData with @StateObject wrapper, so the view updates when the model changes
     @StateObject var recipeData = RecipeData()
     
+    // declare list colors
+    private let listBackgroundColor = AppColor.background
+    private let listTextColor = AppColor.foreground
+    
     var body: some View {
         List {
             ForEach(recipes) { recipe in
                 NavigationLink(recipe.mainInformation.name, destination: RecipeDetailView(recipe: recipe))
             }
+            .listRowBackground(listBackgroundColor)
+            .foregroundStyle(listTextColor) // Using foregroundStyle, as foregroundColor is deprecated
         }
         .navigationTitle(navigationTitle)
     }
