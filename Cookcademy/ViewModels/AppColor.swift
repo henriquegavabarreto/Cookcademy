39f91ff4923c7b430b0b6d5aefa3f72964e03bc1
Henrique Gava <henriquegavabarreto@gmail.com>
2025-07-29 11:25:36 -0700

    Add AppColor with color scheme view model for background and foreground

---
+++ Cookcademy/ViewModels/AppColor.swift
@@ -0,0 +1,23 @@
+//
+//  AppColor.swift
+//  Cookcademy
+//
+//  Created by Henrique Barreto on 7/29/25.
+//
+
+import SwiftUI
+
+// App collor scheme
+struct AppColor {
+    static let background: Color = Color(.sRGB,
+                                         red: 228/255,
+                                         green: 235/255,
+                                         blue: 250/255,
+                                         opacity: 1)
+    
+    static let foreground: Color = Color(.sRGB,
+                                         red: 118/255,
+                                         green: 119/255,
+                                         blue: 231/255,
+                                         opacity: 1)
+}
