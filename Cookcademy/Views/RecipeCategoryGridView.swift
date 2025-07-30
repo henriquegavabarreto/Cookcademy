f55b352501abd233a56293680c5555d42592e606
Henrique Gava <henriquegavabarreto@gmail.com>
2025-07-30 15:58:31 -0700

    adds RecipeCategoryGridView displaying MainInformation categories, adds images corresponding to categories to assets

---
+++ Cookcademy/Views/RecipeCategoryGridView.swift
@@ -0,0 +1,44 @@
+//
+//  RecipeCategoryGridView.swift
+//  Cookcademy
+//
+//  Created by Henrique Barreto on 7/30/25.
+//
+
+import SwiftUI
+
+struct RecipeCategoryGridView: View {
+    private var recipeData = RecipeData()
+    var body: some View {
+        let columns = [GridItem(), GridItem()]
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(MainInformation.Category.allCases, id: \.self) { category in
+                        CategoryView(category: category)
+                    }
+                }
+            }
+            .navigationTitle("Categories")
+        }
+    }
+}
+
+struct CategoryView: View {
+    let category: MainInformation.Category
+    
+    var body: some View {
+        ZStack {
+            Image(category.rawValue)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .opacity(0.35)
+            Text(category.rawValue)
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+#Preview {
+    RecipeCategoryGridView()
+}
