9867dbba1f1ef9085704984745d94b4d1a1d074b
Henrique Gava <henriquegavabarreto@gmail.com>
2025-07-30 16:51:51 -0700

    add recipes function to RecipeData to return recipes filtered by category, refactor RecipesListView to be a child view of RecipeCategoryGridView

--- Cookcademy/ViewModels/RecipeData.swift
+++ Cookcademy/ViewModels/RecipeData.swift
@@ -10,4 +10,16 @@ import Foundation
 // Create RecipeData conforming to ObservableObject so @Published recipes changes can be tracked
 class RecipeData: ObservableObject {
     @Published var recipes = Recipe.testRecipes
+    
+    // return a filtered recipes array based on given category
+    func recipes(for category: MainInformation.Category) -> [Recipe] {
+        var filteredRecipes = [Recipe]()
+        for recipe in recipes {
+            if recipe.mainInformation.category == category {
+                filteredRecipes.append(recipe)
+            }
+        }
+        
+        return filteredRecipes
+    }
 }
