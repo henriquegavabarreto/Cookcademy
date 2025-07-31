87894588ca6df47ac695e76971b47d3372b00344
Henrique Gava <henriquegavabarreto@gmail.com>
2025-07-31 11:01:13 -0700

    adds toolbar button to present sheet with ModifyRecipeView, adds toolbar to sheet with dismiss and add recipe actions

--- Cookcademy/Views/ExploreRecipes/RecipesListView.swift
+++ Cookcademy/Views/ExploreRecipes/RecipesListView.swift
@@ -12,6 +12,11 @@ struct RecipesListView: View {
     @EnvironmentObject private var recipeData: RecipeData
     let category: MainInformation.Category
     
+    // bool that controls sheet presenting status
+    @State private var isPresenting = false
+    // new recipe value to be bound to ModifyRecipeView
+    @State private var newRecipe = Recipe()
+    
     // declare list colors
     private let listBackgroundColor = AppColor.background
     private let listTextColor = AppColor.foreground
@@ -25,6 +30,35 @@ struct RecipesListView: View {
             .foregroundStyle(listTextColor) // Using foregroundStyle, as foregroundColor is deprecated
         }
         .navigationTitle(navigationTitle)
+        .toolbar {
+            // adds button to top right
+            ToolbarItem(placement: .topBarTrailing) {
+                Button(action: {
+                    isPresenting = true
+                }, label: {
+                    Image(systemName: "plus")
+                })
+            }
+        }
+        .sheet(isPresented: $isPresenting) {
+            NavigationView {
+                ModifyRecipeView(recipe: $newRecipe)
+                    .navigationTitle("Add a New Recipe")
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Dismiss") {
+                                isPresenting = false
+                            }
+                        }
+                        ToolbarItem(placement: .confirmationAction) {
+                            Button("Add") {
+                                recipeData.recipes.append(newRecipe)
+                                isPresenting = false
+                            }
+                        }
+                    }
+            }
+        }
     }
 }
 
